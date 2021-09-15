30b6d4d8a93a903be5881aa639aee847c4d792bd
Osysh <max.abylon@gmail.com>
2021-09-15 16:29:11 +0200

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule, MatIconModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/kanban-page/kanban-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-page',
+  templateUrl: './kanban-page.component.html',
+  styleUrls: ['./kanban-page.component.scss']
+})
+export class KanbanPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
