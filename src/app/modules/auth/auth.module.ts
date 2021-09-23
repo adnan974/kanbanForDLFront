b274ec54bace75deefcf3b1b0c38c272bd6639e9
Adnan <adnan.goulamaly@gmail.com>
2021-09-23 12:09:44 +0400

    feature: add form validation for login and register forms

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -16,7 +17,8 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     AuthRoutingModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ]
 })
 export class AuthModule { }
