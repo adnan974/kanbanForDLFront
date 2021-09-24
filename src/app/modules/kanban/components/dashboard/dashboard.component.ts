638f9bce1c55ffe6686d6bd12aba13fab5cfb8af
Osysh <max.abylon@gmail.com>
2021-09-24 15:36:44 +0200

    login

--- src/app/modules/kanban/components/dashboard/dashboard.component.ts
+++ src/app/modules/kanban/components/dashboard/dashboard.component.ts
@@ -1,41 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { TicketService } from 'src/app/core/services/ticket.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
+
 export class DashboardComponent implements OnInit {
 
-  public isLoading:boolean = false
-  public ticketsData: any = {
-    todoStatus: {},
-    inProgressStatus: {},
+  public isLoading: boolean = false
+  public ticketList: { [key: string]: any[] } = {
+    'To Do': [],
+    'In Progress': [],
+    'Closed': []
   }
-
-  constructor(
-    private ticketService: TicketService
-  ) { }
+  
+  constructor(private ticketService: TicketService) { }
 
   ngOnInit(): void {
     this.ticketService.getUserTickets()
-      .subscribe((tickets) => {
-
-        const ticketInTodoStatus = tickets.filter((ticket: any) => {
-          return ticket.ticketStatus == 'Todo'
-        })
-
-        const ticketInProgressStatus = tickets.filter((ticket: any) => {
-          return ticket.ticketStatus == 'In Progress'
-        })
-
-        this.ticketsData.todoStatus = ticketInTodoStatus;
-        this.ticketsData.inProgressStatus = ticketInProgressStatus;
-
+      .subscribe(tickets => {
         this.isLoading = true
-
-
+        console.log(tickets)
+        
+        for (const ticket of tickets) {
+          if (_.isEmpty(this.ticketList[ticket.ticketStatus])) {
+            this.ticketList[ticket.ticketStatus] = [];
+          }
+          this.ticketList[ticket.ticketStatus].push(ticket);
+        }
       })
   }
 
